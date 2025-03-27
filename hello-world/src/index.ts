c093f1307adef941ecc99d87d466a2bb6d1bcfc2
Laura Sanchez <slaura2048@gmail.com>
2025-03-27 03:11:11 -0700

    Declaring and initializing types.

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,4 +1,11 @@
-let age: number = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let sales: number = 123_456_789;
+let course: string = "TypeScript";
+let is_published: boolean = true;
+
+let sales2 = 123_456_789;
+let course2 = "Typescript";
+let is_published2 = true;
+
+let level;
+
+
