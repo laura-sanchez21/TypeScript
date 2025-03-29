c379eda99ea3f405da232fda3583c3896634c7fb
Laura Sanchez <slaura2048@gmail.com>
2025-03-29 03:53:56 -0700

    Learning about functions and enabling compiler options that will be useful.

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,13 +1,14 @@
-// const small = 1;
-// const medium = 2;
-// const large = 3;
+//? makes taxYear an optional argument, you dont need to pass in taxYear when calling calculateTax()
+//function calculateTax(income: number, taxYear?: number): number{
+function calculateTax(income: number, taxYear: number = 2022): number {
+    // let x; warning is emitted as x it is not used
+    //if (income < 50_000)
+    //if(taxYear < 2022) //taxYear will complain because of the ? to solve this do the following
+    //if((taxYear || 2022) < 2022) //this is one approach the second is to define taxYear in the function
+    if(taxYear < 2022)
+    return income * 1.2;
+    return income * 1.3;
+}
 
-// enum Size { Small = 's', Meduium = 'm', Large = 'l'};
-
-// enum SizeTwo { Small = 1, Meduium, Large};
-// let mySize = SizeTwo.Meduium;
-// console.log(mySize);
-
-const enum SizeTwo { Small = 1, Meduium, Large};
-let mySize = SizeTwo.Meduium;
-console.log(mySize);
+//calculateTax(10_000, 2022, 1);
+calculateTax(10_000, 2022);
