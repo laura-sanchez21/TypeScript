65c3b06d37c2baf14d5e0da7cbe41dc4630f8e6f
Laura Sanchez <slaura2048@gmail.com>
2025-03-30 01:20:58 -0700

    Learning about objects.

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,14 +1,14 @@
-//? makes taxYear an optional argument, you dont need to pass in taxYear when calling calculateTax()
-//function calculateTax(income: number, taxYear?: number): number{
-function calculateTax(income: number, taxYear: number = 2022): number {
-    // let x; warning is emitted as x it is not used
-    //if (income < 50_000)
-    //if(taxYear < 2022) //taxYear will complain because of the ? to solve this do the following
-    //if((taxYear || 2022) < 2022) //this is one approach the second is to define taxYear in the function
-    if(taxYear < 2022)
-        return income * 1.2;
-    return income * 1.3;
-}
+let employee: {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+} = { 
+    id: 1,
+    name: "Laura",
+    retire: (date: Date) => {
+        console.log(date);
+    }
+};
 
-//calculateTax(10_000, 2022, 1);
-calculateTax(10_000, 2022);
+//employee.name = "Laura";
+//employee.id = 3; //you cant modify id as id property is readonly.
