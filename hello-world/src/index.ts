3f043a3d3bc7eb1ee7bfc1acd5537361f9beb413
Laura Sanchez <slaura2048@gmail.com>
2025-03-31 00:59:26 -0700

    Excercise 1: Testing introductory level material, such as, functions, different types, and objects

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,14 +1,39 @@
-let employee: {
-    readonly id: number,
-    name: string,
-    retire: (date: Date) => void
-} = { 
-    id: 1,
-    name: "Laura",
-    retire: (date: Date) => {
-        console.log(date);
-    }
-};
+//For each of these values, what type will the TypeScript compiler infer?
 
-//employee.name = "Laura";
-//employee.id = 3; //you cant modify id as id property is readonly.
+//number
+let a = 100;
+
+//string
+let b = 'Coffee';
+
+//boolean[]
+let c = [true, false, false];
+
+//object age: number
+let d = {age: number};
+
+//number[]
+let e = [3];
+
+//amy
+let f;
+
+//any[]
+let g = [];
+
+//What are the compilation errors in each of the following code snippets?
+let song: {
+title: string,
+releaseYear: number
+} = { title: 'My song' };
+//missing releaseYear initialization
+//= { title: 'My song'. releaseYear: 2025 };
+
+let prices = [100, 200, 300];
+prices[0] = '$100';
+//prices is a number[], you are trying to pass a string into prices[0],
+//instead pass in a number
+
+function myFunc(a: number, b: number): number { }
+//myFunc is supposed to return a number, there is nothing currently being returned in this function.
+//number { return a + b; }
