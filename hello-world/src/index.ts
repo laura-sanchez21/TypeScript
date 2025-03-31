857ba1a22568b778fedbde2c1c6b90f4103c466e
Laura Sanchez <slaura2048@gmail.com>
2025-03-31 03:25:14 -0700

    Learning about Type Aliases

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,39 +1,43 @@
-//For each of these values, what type will the TypeScript compiler infer?
+type Employee = {
+    readonly id: number;
+    name: string;
+    retire: (date: Date) => void;
+}
 
-//number
-let a = 100;
+type ExtendedEmployee = Employee & {
+    promote: () => void;
+}
 
-//string
-let b = 'Coffee';
+let employee: Employee = {
+    id: 1,
+    name: "Laura",
+    retire: (date: Date) => {
+        console.log(date);
+    }
+}
 
-//boolean[]
-let c = [true, false, false];
+let extendedEmployee: ExtendedEmployee = {
+    id: 1,
+    name: "Laura",
+    retire: (date: Date) => {
+        console.log(date);
+    },
+    promote: () =>  {
+        console.log("Employee promoted");
+    }
 
-//object age: number
-let d = {age: number};
+}
 
-//number[]
-let e = [3];
+// let employee: {
+//     readonly id: number,
+//     name: string,
+//     retire: (date: Date) => void
+// } = {
 
-//amy
-let f;
-
-//any[]
-let g = [];
-
-//What are the compilation errors in each of the following code snippets?
-let song: {
-title: string,
-releaseYear: number
-} = { title: 'My song' };
-//missing releaseYear initialization
-//= { title: 'My song'. releaseYear: 2025 };
-
-let prices = [100, 200, 300];
-prices[0] = '$100';
-//prices is a number[], you are trying to pass a string into prices[0],
-//instead pass in a number
-
-function myFunc(a: number, b: number): number { }
-//myFunc is supposed to return a number, there is nothing currently being returned in this function.
-//number { return a + b; }
+//using object expansion to expand Employee
+// const extendedEmployee = {
+//     ...employee,
+//     promoted: () => {
+//         console.log("Employee has been promoted");
+//     }
+// }
