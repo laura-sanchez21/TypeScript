f43eabd5211842a3ecc8a49f0edc0be8c49e2d01
Laura Sanchez <slaura2048@gmail.com>
2025-04-01 19:00:33 -0700

    Learning about Nullable Types

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -1,15 +1,9 @@
 "use strict";
-//missing :numner as it is only needed for the compiler, to type-chaeck
-function kgToLbs(weight) {
-    if (typeof weight == 'number') {
-        return weight * 2.2;
-    }
-    else {
-        return parseInt(weight) * 2.2;
-    }
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log("Hola!");
 }
-;
-kgToLbs(19);
-kgToLbs("100kg");
-kgToLbs('10kg');
+greet(null);
 //# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,9 +1,8 @@
-//let quanitiy = 100;
+function greet (name: string | null | undefined) {
+    if(name)
+        console.log(name.toUpperCase);
+    else
+        console.log("Hola!");
+}
 
-//we want the quantity to be 50 or 100
-//let quantity: number; //instead of initializing quanitity to a number
-
-//Literal (exact, specific)
-//let quantity: 50 | 100 = 51
-type Quantity =  50 | 100;
-let quantity: Quantity = 100;
+greet(undefined);
