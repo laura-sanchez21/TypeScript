e71c9d1924c6b3b80ac224f6d3916aaab25340f8
Laura Sanchez <slaura2048@gmail.com>
2025-04-04 02:14:14 -0700

    Learning about Optional Chaining

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,8 +1,22 @@
-function greet (name: string | null | undefined) {
-    if(name)
-        console.log(name.toUpperCase);
-    else
-        console.log("Hola!");
+type Customer = {
+    birthday?: Date;
 }
 
-greet(undefined);
+function getCustomer(id: number): Customer | null | undefined{
+    return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(1);
+// if(customer !== null && customer !== undefined)
+//Optional property access operator
+console.log(customer?.birthday?.getFullYear());
+
+//Optional element access operator
+//if(customers !== null && customers !== undefined)
+    //customers?. [0]
+
+//Optional call
+//let log: any = (message: string) => console.log(message);
+let log: any = null;
+log?.('a')
+
