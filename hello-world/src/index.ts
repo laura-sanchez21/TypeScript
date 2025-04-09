80753e4b69367433ae6490da692f4ad76642db32
Laura Sanchez <slaura2048@gmail.com>
2025-04-09 02:54:25 -0700

    Learning about unkown type

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,7 +1,15 @@
-//as keyword has nothing to do with type conversionn
-//let phone = document.getElementById('phone') as HTMLInputElement;
-let phone = <HTMLInputElement> document.getElementById('phone');
-//HTMLElement
-//HTMLInputElement
+//function render(document: any) {
+function render(document: unknown) {
+    //Norrowing
+    // if(typeof document === 'string') {
+    //     document.toUpperCase();
+    // }
 
-phone.value
+    //used for custom objects
+    if (document instanceof WordDocument) {
+        document.toUpperCase();
+    }
+    
+    document.move();
+    document.fly();
+}
