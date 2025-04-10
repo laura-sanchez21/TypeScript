1b3cd58f2668cee5cde6f85be503c9f4f35df062
Laura Sanchez <slaura2048@gmail.com>
2025-04-10 00:36:42 -0700

    Learning abnout never types

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,15 +1,13 @@
-//function render(document: any) {
-function render(document: unknown) {
-    //Norrowing
-    // if(typeof document === 'string') {
-    //     document.toUpperCase();
-    // }
-
-    //used for custom objects
-    if (document instanceof WordDocument) {
-        document.toUpperCase();
-    }
-    
-    document.move();
-    document.fly();
+function reject(message: string): never {
+    throw new Error(message);
 }
+
+function processEvents(): never {
+    while(true){
+        //Read a message from a queue
+    }
+}
+
+reject('...');
+//processEvents();
+console.log("Hello World");
