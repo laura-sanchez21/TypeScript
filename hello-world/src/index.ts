d936bbaf3659930ebacf8d63cd38d352113159bb
Laura Sanchez <slaura2048@gmail.com>
2025-04-15 17:38:14 -0700

    Completed Excercise 2

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,13 +1,51 @@
-function reject(message: string): never {
-    throw new Error(message);
+// Given the data below, define a type alias for representing users.
+// let users = [
+//     {
+//         name: 'John Smith',
+//         age: 30,
+//         occupation: 'Software engineer'
+//     },
+//     {
+//         name: 'Kate Müller',
+//         age: 28
+//     }
+// ];
+
+type User = {
+    name: string;
+    age: number;
+    occupation?: string;
 }
 
-function processEvents(): never {
-    while(true){
-        //Read a message from a queue
-    }
+//Birds fly. Fish swim. A Pet can be a Bird or Fish. Use type aliases to represent these
+type Bird = {
+    type: "bird";
+    fly: () => void;
 }
 
-reject('...');
-//processEvents();
-console.log("Hello World");
+type Fish = {
+    type: "fish";
+    swim: () => void;
+}
+
+type Pet = Bird | Fish;
+
+//Define a type for representing the days of week. Valid values are “Monday”, “Tuesday”,
+//etc
+type DayOfWeek = 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday';
+
+//Simplify the following code snippets:
+// let user = getUser();
+// console.log(user && user.address ? user.address.street : undefined);
+console.log(user?.address?.street);
+
+// let x = foo !== null && foo !== undefined ? foo : bar()
+let x = foo ?? bar()
+
+//What is the problem in this piece of code?
+let value: unknown = 'a';
+if (typeof value === 'string')
+    console.log(value.toUpperCase());
+
+//when a type is unkown you have to check the type to use functionn calls like toUpperCase
+//as it is only related to strings
