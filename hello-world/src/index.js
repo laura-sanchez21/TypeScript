c1fb4544dc57db76b57a2ffec4e650bc50c5ea2d
Laura Sanchez <slaura2048@gmail.com>
2025-04-16 22:01:43 -0700

    Learning about classes

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,14 +1,13 @@
-//? makes taxYear an optional argument, you dont need to pass in taxYear when calling calculateTax()
-//function calculateTax(income: number, taxYear?: number): number{
-function calculateTax(income, taxYear) {
-    if (taxYear === void 0) { taxYear = 2022; }
-    // let x; warning is emitted as x it is not used
-    //if (income < 50_000)
-    //if(taxYear < 2022) //taxYear will complain because of the ? to solve this do the following
-    //if((taxYear || 2022) < 2022) //this is one approach the second is to define taxYear in the function
-    if (taxYear < 2022)
-        return income * 1.2;
-    return income * 1.3;
-}
-//calculateTax(10_000, 2022, 1);
-calculateTax(10000, 2022);
+var Account = /** @class */ (function () {
+    function Account(id, owner, balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+    Account.prototype.deposit = function (amount) {
+        if (amount <= 0)
+            throw new Error("Invalid amount");
+        this.balance += amount;
+    };
+    return Account;
+}());
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,85 +1,18 @@
-// //Part 1
-// type Employee = {
-//     hame: string;
-//     age: number;
-//     department ?: string;
-//     isActive : boolean;
-// }
+class Account {
+    id: number;
+    owner: string;
+    balance: number;
 
-// //Part 2
-// type Car = {
-//     wheels: number;
-//     drive(): void;
-// }
-
-// type Bike = {
-//     wheels: number;
-//     pedal(): void;
-// }
-
-// type Vehicle = Car | Bike;
-
-// //Part 3
-// let user = {
-//     profile: {
-//         address: {
-//             city: "Berlin"
-//         }
-//     }
-// };
-// console.log(user?.profile?.address?.city);
-
-// //part 4
-// // let name: string | null = null;
-// // let displayName = name ?? "Guest";
-
-// //Part 5
-// let score = 42;        // number
-// let title = "Quiz!";   // string
-// let flags = [true];    // boolean[]
-// let empty = [];        // any[]
-
-// //Part 6
-// let movie: {
-//     title: string,
-//     duration: number
-//   } = {
-//     title: "Epic Movie"
-//   };
-
-//   //Need to initialize duration
-
-//   //Part 7
-//   let input: unknown = "typescript";
-
-//   if (typeof input ===  'string')
-//         console.log(input.toUpperCase());
-
-type Car = {
-    type: "car";
-    wheels: 4;
-    drive(): void;
-}
-
-type Bike = {
-    type: "bike";
-    wheels: 2;
-    pedal(): void;
-}
-
-type Vehicle = Car | Bike;
-
-// Your answer:
-function handleVehicle(vehicle: Vehicle): void {
-    switch(vehicle.type) {
-        case 'bike':
-            vehicle.pedal();
-            break;
-        case 'car':
-            vehicle.drive();
-            break;
-        default:
-            break;
+    constructor(id: number, owner: string, balance: number) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
     }
-    console.log("Using a vehicle with X wheels " + vehicle.wheels );
+
+    deposit(amount: number): void {
+        if (amount <= 0)
+            throw new Error("Invalid amount");
+        
+        this.balance += amount;
+    }
 }
