6526759cae688111296366b0aef77b169b243d72
Laura Sanchez <slaura2048@gmail.com>
2025-04-17 02:00:43 -0700

    Creating objects

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -6,8 +6,16 @@ var Account = /** @class */ (function () {
     }
     Account.prototype.deposit = function (amount) {
         if (amount <= 0)
-            throw new Error("Invalid amount");
+            throw new Error("Invalid amount!");
         this.balance += amount;
     };
     return Account;
 }());
+var account = new Account(1, "Laura", 0);
+account.deposit(100);
+console.log("Account balance: " + account.balance);
+console.log(account);
+console.log("Typeof " + typeof account);
+console.log("Instance of Account: " + (account instanceof Account));
+//union
+//if (typeof someObj === 'number')
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -3,16 +3,25 @@ class Account {
     owner: string;
     balance: number;
 
-    constructor(id: number, owner: string, balance: number) {
+    constructor (id: number, owner: string, balance: number) {
         this.id = id;
         this.owner = owner;
         this.balance = balance;
     }
 
     deposit(amount: number): void {
-        if (amount <= 0)
-            throw new Error("Invalid amount");
-        
+        if(amount <= 0)
+            throw new Error("Invalid amount!");
         this.balance += amount;
     }
 }
+
+let account = new Account(1, "Laura", 0);
+account.deposit(100);
+console.log("Account balance: " + account.balance);
+console.log(account);
+console.log("Typeof " + typeof account)
+console.log("Instance of Account: " +(account instanceof Account));
+
+//union
+//if (typeof someObj === 'number')
