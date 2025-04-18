d1b25fd73f44aade5c6769f3873a2185601ea114
Laura Sanchez <slaura2048@gmail.com>
2025-04-17 17:19:29 -0700

    Learning about Access Control Keywords

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,28 +1,39 @@
 class Account {
     readonly id: number;
     owner: string;
-    balance: number;
+    private _balance: number;
     nickname?: string;
 
     constructor (id: number, owner: string, balance: number) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
 
     deposit(amount: number): void {
         //this.id = 0;
         if(amount <= 0)
             throw new Error("Invalid amount!");
-        this.balance += amount;
+        //Record a transaction
+        this._balance += amount;
+    }
+
+    getBalance(): number {
+        return this._balance;
+    }
+
+    private calculateTax(): void {
+
     }
 }
 
 let account = new Account(1, "Laura", 0);
-//account.id = 0;
+//account.id = 0; error: id is readonly
+//account.balance -= 1; error: balance is private
+console.log(account.getBalance);
 
 account.deposit(100);
-console.log("Account balance: " + account.balance);
+//console.log("Account balance: " + account.balance);
 console.log(account);
 console.log("Typeof " + typeof account)
 console.log("Instance of Account: " +(account instanceof Account));
