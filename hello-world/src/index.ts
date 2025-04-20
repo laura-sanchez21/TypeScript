67c68efb47dc0bdfaeb6e944df7d0ab8e79f7c91
Laura Sanchez <slaura2048@gmail.com>
2025-04-19 22:09:17 -0700

    Learning about index signatures

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,52 +1,16 @@
-class Account {
-    nickname?: string;
-
-    constructor (
-        public readonly id: number, 
-        public owner: string, 
-        private _balance: number) {
-    }
-
-    deposit(amount: number): void {
-        //this.id = 0;
-        if(amount <= 0)
-            throw new Error("Invalid amount!");
-        //Record a transaction
-        this._balance += amount;
-    }
-
-    // getBalance(): number {
-    //     return this._balance;
-    // }
-
-    get balance(): number {
-        return this._balance;
-    }
-
-    // set balance(value: number) 
-    // {
-    //     if(value <= 0) {
-    //         throw Error("Invalid value!");
-    //     }
-
-    //     this._balance = value;
-    // }
-
-    private calculateTax(): void {
-    }
+class SeatAssignment {
+   //index signature properties
+   //using a number
+   //[seatNumber: number]: string;
+   [seatNumber: string]: string;
 }
 
-let account = new Account(1, "Laura", 0);
-//account.id = 0; error: id is readonly
-//account.balance -= 1; error: balance is private
-console.log(account.balance);
-//account.balance = 1;
+//let seats = new  SeatAssignment();
+//initializing 1 with 'm'
+//seats[1] = "m"
 
-account.deposit(100);
-//console.log("Account balance: " + account.balance);
-console.log(account);
-console.log("Typeof " + typeof account)
-console.log("Instance of Account: " +(account instanceof Account));
-
-//union
-//if (typeof someObj === 'number')
+let seats = new SeatAssignment();
+seats.A1 = "Mosh";
+//seats['A1'] = "Mosh"; identical to the above line
+seats.A2 = "John";
+seats.A2 = 1; 
