c1c06cc542f84b43a0fbe19965bf8040b6c0b3c4
Laura Sanchez <slaura2048@gmail.com>
2025-04-21 01:31:00 -0700

    learning about overriding. There is a current issue with super.fullName

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,16 +1,60 @@
-var Ride = /** @class */ (function () {
-    function Ride() {
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Person = /** @class */ (function () {
+    function Person(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
-    //constructor(public activeRides: number) {
-    //}
-    Ride.prototype.start = function () { Ride.activeRides++; };
-    Ride.prototype.stop = function () { Ride.activeRides--; };
-    Ride.activeRides = 0;
-    return Ride;
+    Person.prototype.walk = function () { console.log("Walking"); };
+    Person.prototype.talk = function () { console.log("Talking"); };
+    Object.defineProperty(Person.prototype, "fullName", {
+        get: function () {
+            return this.firstName + " " + this.lastName;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Person;
 }());
-var ride1 = new Ride;
-ride1.start();
-var ride2 = new Ride();
-ride2.start();
-console.log("Ride1: " + Ride.activeRides);
-console.log("Ride2: " + Ride.activeRides);
+// class Student extends Person {
+//    constructor(
+//       public studentId: number, 
+//       firstName: string, 
+//       lastName: string) {
+//          super(firstName, lastName);
+//       }
+//    takeTest() { console.log("Taking a test!"); }
+// }
+var Teacher = /** @class */ (function (_super) {
+    __extends(Teacher, _super);
+    function Teacher(firstName, lastName) {
+        return _super.call(this, firstName, lastName) || this;
+    }
+    Object.defineProperty(Teacher.prototype, "fullName", {
+        //we do not need override as we are just adding "Professor"
+        get: function () {
+            return "Professor " + this.firstName + " " + this.lastName;
+            //return "Professor " + super.fullName;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Teacher.prototype.walk = function () { console.log("Professor is Walking"); };
+    return Teacher;
+}(Person));
+var teacher = new Teacher("John", "Doe");
+console.log(teacher.fullName);
+teacher.walk();
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -7,24 +7,37 @@ class Person {
 
       talk() { console.log("Talking"); }
 
-      get fullName() {
+      public get fullName() {
          return this.firstName + " " + this.lastName;
       }
 }
 
-class Student extends Person {
-   constructor(
-      public studentId: number, 
-      firstName: string, 
-      lastName: string) {
-         super(firstName, lastName);
-      }
+// class Student extends Person {
+//    constructor(
+//       public studentId: number, 
+//       firstName: string, 
+//       lastName: string) {
+//          super(firstName, lastName);
+//       }
 
-   takeTest() {
-      console.log("Taking a test!");
+//    takeTest() { console.log("Taking a test!"); }
+// }
+
+class Teacher extends Person {
+
+   constructor(firstName: string, lastName: string) {
+      super(firstName, lastName);
    }
+
+   //we do not need override as we are just adding "Professor"
+   override get fullName() {
+      return "Professor " + this.firstName + " " + this.lastName;
+      //return "Professor " + super.fullName;
+   }
+
+   override walk() { console.log("Professor is Walking"); }
 }
 
-let student = new Student(123, "John", "Doe");
-student.talk();
-student.takeTest();
+let teacher = new Teacher("John", "Doe");
+console.log(teacher.fullName);
+teacher.walk();
