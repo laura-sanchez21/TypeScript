09a70894e27980e3da071ee133b3caa70805ca61
Laura Sanchez <slaura2048@gmail.com>
2025-04-23 00:53:13 -0700

    Learning about abstract classes and methods.

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,65 +1,20 @@
-class Person {
-   constructor(
-      public firstName: string, 
-      public lastName: string) { }
+abstract class Shape {
+   constructor(public color: string) {
 
-      private walk() { console.log("Walking"); }
-
-      protected talk() { console.log("Talking"); }
-
-      public get fullName() {
-         return this.firstName + " " + this.lastName;
-      }
+   }
+   abstract render(): void;
 }
 
-class Student extends Person {
-   constructor(
-      public studentId: number, 
-      firstName: string, 
-      lastName: string) {
-         super(firstName, lastName);
-      }
-   
-      override talk() {
-         console.log("Student is talking")
-      }
+class Circle extends Shape {
+   constructor(public radius: number, color: string) {
+      super(color); 
+   }
 
-   takeTest() { 
-      this.talk();
-      console.log("Taking a test!"); 
+   override render() { 
+      console.log("Rendering a Circle");
    }
 }
 
-//base class wont allow for the object to use talk as it is protected
-// let person = new Person("tom", "riddle");
-// person.talk()
-
-//child class allows for talk to be called as it is considered public in
-//the student class
-// let student = new Student(123, "tomm", "tom");
-// student.talk();
-
-class Teacher extends Person {
-   override get fullName() {
-      return "Professor " + this.firstName + " " + this.lastName;
-   }
-}
-
-class Principal extends Person {
-   override get fullName() {
-      return "Principal " + this.firstName + " " + this.lastName;
-   }
-}
-
-printName([
-   new Student(123, "John", "doe"),
-   new Teacher("foo", "doo"),
-   new Principal("Tom", "Soe")
-])
-
-function printName(people: Person[]) {
-   for ( let person of people) {
-      console.log(person.fullName);
-      
-   }
-}
+//rendering a shape makes 0 sense so make the class an abstract
+//let shape = new Shape("red");
+//shape.render();
