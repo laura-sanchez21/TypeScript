ab5b90a455e43ae9f3fae4bce8264d1cdead49d1
Laura Sanchez <slaura2048@gmail.com>
2025-04-24 00:26:33 -0700

    Learning about Generic Functions

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,11 +1,26 @@
-class KeyValuePairs<T, U> {
-   constructor(public key: T, public value: U) {
+///Original
+function wrapInArray1(value: number) {
+   return [value];
+}
 
+let numbers = wrapInArray1(1);
+
+//Generic Type
+function wrapInArray2<T>(value: T) {
+   return [value];
+}
+
+let numbers2 = wrapInArray2(1);
+let numbers3 = wrapInArray2('1');
+
+class ArrayUtils {
+   static wrapInArray3<T>(value: T) {
+      return [value];
    }
 }
 
-let pair = new KeyValuePairs(6, "hello");
-pair.key.toPrecision();
+// let utils = new ArrayUtils();
+// let numbers4 = utils.wrapInArray3(5);
 
-let pair2 = new KeyValuePairs<string, string>("temp", "three");
-pair2.key.charAt(0);
+//Making the function static
+let numbers4 = ArrayUtils.wrapInArray3(5);
